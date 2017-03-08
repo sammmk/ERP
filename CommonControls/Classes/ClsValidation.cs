9e6e95b4c502402538cf1d434a8aa971250dd791
sammmk <moha.mmk@gmail.com>
2017-03-08 17:14:52 +0900

    Add Common Controls

---
+++ CommonControls/Classes/ClsValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonControls.Classes
+{
+    public class ClsValidation
+    {
+        private bool isLetterOnly()
+        {
+            return true;
+        }
+    }
+}
